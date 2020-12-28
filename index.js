fb96d19f12a6b8031e0559a4a9414afee2f6b35f
Naor Tedgi <naor.tedgi@gmail.com>
2020-12-28 09:47:11 +0200

    finish loading model

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 const tf = require('@tensorflow/tfjs-node');
 
-const model_path = 'https://raw.githubusercontent.com/ntedgi/efficientnet/main/lib/tfjs/model.json'
+const model_path = 'https://raw.githubusercontent.com/ntedgi/efficientnet/main/lib/tfjs/web_model/model.json'
 const load = async () => {
 
-  const model = await tf.loadLayersModel(model_path);
-  model.summary()
+  const model = await tf.loadGraphModel(model_path);
   return model
 }
 
 load().then(model => {
-  console.log(model)
+
+  model.predict()
 })
--- lib/download_models.py
+++ lib/download_models.py
@@ -1,2 +1,4 @@
+import tensorflow as tf
 from tensorflow.keras.applications import EfficientNetB0
 model = EfficientNetB0(weights='imagenet')
+#https://storage.googleapis.com/keras-applications/efficientnetb0.h5
