64670077fddd0e96d2aac70b61f1700ff545d3f1
Naor Tedgi <naor.tedgi@gmail.com>
2020-12-29 13:46:26 +0200

    adding crop and resizre logic

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
+const tfnode = require('@tensorflow/tfjs-node');
+const Jimp = require('jimp');
+let jsonData = require('./misc/labels_map.json');
+const modelPath = 'https://raw.githubusercontent.com/ntedgi/efficientnet/main/lib/tfjs/web_model/model.json';
+const IMAGE_FILE_PATH = "./panda.jpg";
+const IMAGE_SIZE = 224;
+const NUM_OF_CHANNELS = 3;
 
-const tf = require('@tensorflow/tfjs-node');
+const loadLocalImage = async (filename) => {
+    const image = await Jimp.read(filename);
+    const width = image.bitmap.width
+    const height = image.bitmap.height
+    const cropPadding = 32
+    const paddedCenterCropSize = (IMAGE_SIZE / (IMAGE_SIZE + cropPadding)) * Math.min(height, width)
+    const offsetHeight = (((height - paddedCenterCropSize) + 1) / 2 >> 0)
+    const offsetWidth = (((width - paddedCenterCropSize) + 1) / 2 >> 0)
+    await image.crop(offsetWidth, offsetHeight, paddedCenterCropSize, paddedCenterCropSize)
+    await image.resize(IMAGE_SIZE, IMAGE_SIZE, Jimp.RESIZE_BICUBIC);
+    await image.writeAsync(`test/${Date.now()}_crop_${IMAGE_SIZE}x${IMAGE_SIZE}.png`);
+    let values = new Float32Array(IMAGE_SIZE * IMAGE_SIZE * NUM_OF_CHANNELS);
+    let i = 0;
+    const mean = [0.485, 0.456, 0.406]
+    const std = [0.229, 0.224, 0.225]
+    image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+        const pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
+        pixel.r = pixel.r / 127.0 - 1;
+        pixel.g = pixel.g / 127.0 - 1;
+        pixel.b = pixel.b / 127.0 - 1;
+        pixel.a = pixel.a / 127.0 - 1;
+        // values[i * NUM_OF_CHANNELS + 0] = (pixel.r - mean[0]) / std[0]
+        // values[i * NUM_OF_CHANNELS + 1] = (pixel.g - mean[1]) / std[1]
+        // values[i * NUM_OF_CHANNELS + 2] = (pixel.b - mean[2]) / std[2]
+        values[i * NUM_OF_CHANNELS + 0] = pixel.r;
+        values[i * NUM_OF_CHANNELS + 1] = pixel.g;
+        values[i * NUM_OF_CHANNELS + 2] = pixel.b;
+        i++;
+    });
 
-const model_path = 'https://raw.githubusercontent.com/ntedgi/efficientnet/main/lib/tfjs/web_model/model.json'
-const load = async () => {
-
-  const model = await tf.loadGraphModel(model_path);
-  return model
+    const outShape = [IMAGE_SIZE, IMAGE_SIZE, NUM_OF_CHANNELS];
+    let img_tensor = tfnode.tensor3d(values, outShape, 'float32');
+    img_tensor = img_tensor.expandDims(0);
+    const img_tensorAsarray = img_tensor.dataSync()
+    console.log(img_tensorAsarray)
+    return img_tensor
 }
 
-load().then(model => {
+const load = async () => {
+    const model = await tfnode.loadGraphModel(modelPath);
+    return model
+}
 
-  model.predict()
+load().then(async model => {
+
+    const result = await loadLocalImage(IMAGE_FILE_PATH)
+    const objectArray = model.predict(result)
+    const values = objectArray.dataSync();
+    const arr = Array.from(values);
+    const topValues = values.sort((a, b) => b - a).slice(0, 5);
+    const indexes = topValues.map(e => arr.indexOf(e))
+    indexes.forEach(index => {
+        console.log(`${index} [ ${arr[index]} ] : ${jsonData[index]}`)
+
+    })
+
 })
--- lib/download_models.py
+++ lib/download_models.py
@@ -1,4 +1,20 @@
-import tensorflow as tf
-from tensorflow.keras.applications import EfficientNetB0
+import sys
+import numpy as np
+from skimage.io import imread
+sys.path.append('..')
+
+from keras.applications.imagenet_utils import decode_predictions
+from efficientnet.keras import EfficientNetB0
+from efficientnet.keras import center_crop_and_resize, preprocess_input
 model = EfficientNetB0(weights='imagenet')
+
+image = imread('../misc/panda.jpg')
+image_size = model.input_shape[1]
+x = center_crop_and_resize(image, image_size=image_size)
+# x = preprocess_input(x)
+x = np.expand_dims(x, 0)
+
+y = model.predict(x)
+print(decode_predictions(y))
+
 #https://storage.googleapis.com/keras-applications/efficientnetb0.h5
