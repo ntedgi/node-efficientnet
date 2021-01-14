45e3759eb0dcad41b0c2ccac5e8703f2dd302b20
Naor Tedgi <naor.tedgi@gmail.com>
2021-01-14 15:54:30 +0200

    adding example inside playground/server

--- playground/ui/src/app/uploader/uploader.component.ts
+++ playground/ui/src/app/uploader/uploader.component.ts
@@ -1,42 +1,34 @@
-import { Component, OnInit } from "@angular/core";
-import Prediction from "../interfces";
+import { Component, OnInit } from '@angular/core';
+import Prediction from '../interfces';
 
 @Component({
-  selector: "app-uploader",
-  templateUrl: "./uploader.component.html",
-  styleUrls: ["./uploader.component.scss"]
+  selector: 'app-uploader',
+  templateUrl: './uploader.component.html',
+  styleUrls: ['./uploader.component.scss'],
 })
 export class UploaderComponent implements OnInit {
-
-  constructor() {
-  }
+  constructor() {}
 
   image2Display: string | ArrayBuffer;
   classifications: Prediction[];
   loading: boolean;
 
   updateImage(imgPath: File) {
-    console.log(imgPath);
     const reader = new FileReader();
     reader.readAsDataURL(imgPath);
-    debugger
     reader.onload = (_event) => {
-      debugger
       this.image2Display = reader.result;
     };
-
   }
   isLoading(loading: boolean) {
     this.loading = loading;
   }
 
   updateClassification(result: Prediction[]) {
-    console.log("updateClassification");
+    console.log('updateClassification');
     console.log(result);
     this.classifications = result;
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
