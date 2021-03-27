d9b0b8e6216cf5959b439d5ac7698e904dc7209e
luoye <luoyefe@gmail.com>
2021-03-27 20:32:28 +0800

    test: migrate from babel to ts for jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 // jest.config.js
 // Sync object
 module.exports = {
-    verbose: true,
-    testTimeout: 30000,
-    "transform": {
-        "^.+\\.ts$": "babel-jest"
-    },
-    modulePathIgnorePatterns: ["<rootDir>/playground/"]
-
+  verbose: true,
+  testTimeout: 30000,
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  modulePathIgnorePatterns: ["<rootDir>/playground/"],
 };
-
--- tests/model.test.ts
+++ tests/model.test.ts
@@ -1,10 +1,10 @@
-const path = require("path");
-const shelljs = require("shelljs");
+import * as path from "path";
+import * as shelljs from "shelljs";
 
-const {
+import {
   EfficientNetCheckPoint,
   EfficientNetCheckPointFactory,
-} = require("../index");
+} from "../index";
 
 test("EfficientNetCheckPointFactory - create checkpoint B0 without throwing exception", (done) => {
   EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0)
