779f1ad0b263fe89cfe2eecce7659f2e3960eb49
Naor Tedgi <naor.tedgi@gmail.com>
2021-03-27 10:18:48 +0300

    change test to load from local

--- tests/model.test.js
+++ tests/model.test.js
@@ -6,8 +6,24 @@ const {
   EfficientNetCheckPointFactory,
 } = require("../index");
 
+const rootDir = path.join(__dirname, "../lib/tfjs/web_model");
+
+beforeAll((done) => {
+  shelljs.mkdir("-p", path.join(rootDir, "B0"));
+  shelljs.cp("-r", path.join(rootDir, "*.json"), path.join(rootDir, "B0"));
+  shelljs.cp("-r", path.join(rootDir, "*.bin"), path.join(rootDir, "B0"));
+  done();
+});
+
+afterAll((done) => {
+  shelljs.rm("-rf", path.join(rootDir, "B0"));
+  done();
+});
+
 test("EfficientNetCheckPointFactory - create checkpoint B0 without throwing exception", (done) => {
-  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0)
+  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0, {
+    localModelRootDirectory: rootDir,
+  })
     .then((model) => {
       expect(model).toBeDefined();
       expect(model).toHaveProperty("modelPath");
@@ -19,7 +35,9 @@ test("EfficientNetCheckPointFactory - create checkpoint B0 without throwing exce
 });
 
 test("EfficientNetCheckPointFactory - checkpoint B0 should predict panda at top precision without throwing exception", (done) => {
-  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0)
+  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0, {
+    localModelRootDirectory: rootDir,
+  })
     .then(async (model) => {
       expect(model).toBeDefined();
       const image = "samples/panda.jpg";
@@ -34,7 +52,9 @@ test("EfficientNetCheckPointFactory - checkpoint B0 should predict panda at top 
 });
 
 test("EfficientNetCheckPointFactory - checkpoint B0 should predict car at top precision without throwing exception", (done) => {
-  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0)
+  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0, {
+    localModelRootDirectory: rootDir,
+  })
     .then(async (model) => {
       expect(model).toBeDefined();
       const image = "samples/car.jpg";
@@ -47,7 +67,9 @@ test("EfficientNetCheckPointFactory - checkpoint B0 should predict car at top pr
 });
 
 test("EfficientNetCheckPointFactory - checkpoint B0 should return top 5 answers", (done) => {
-  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0)
+  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0, {
+    localModelRootDirectory: rootDir,
+  })
     .then(async (model) => {
       expect(model).toBeDefined();
       const image = "samples/car.jpg";
@@ -61,26 +83,35 @@ test("EfficientNetCheckPointFactory - checkpoint B0 should return top 5 answers"
 });
 
 test("EfficientNetCheckPointFactory - load model from local file", (done) => {
-  const rootDir = path.join(__dirname, "../lib/tfjs/web_model");
-
-  shelljs.mkdir("-p", path.join(rootDir, "B0"));
-  shelljs.cp("-r", path.join(rootDir, "*.json"), path.join(rootDir, "B0"));
-  shelljs.cp("-r", path.join(rootDir, "*.bin"), path.join(rootDir, "B0"));
-
   EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0, {
-    localModelRootDirectory: path.join(__dirname, "../lib/tfjs/web_model"),
+    localModelRootDirectory: rootDir,
   })
     .then(async (model) => {
       expect(model).toBeDefined();
       const image = "samples/car.jpg";
       model.inference(image).then((predictions) => {
         expect(predictions.result[0].label).toEqual("sports car, sport car");
-        shelljs.rm("-rf", path.join(rootDir, "B0"));
         done();
       });
     })
     .catch((error) => {
-      shelljs.rm("-rf", path.join(rootDir, "B0"));
+      done(error);
+    });
+});
+
+test("EfficientNetCheckPointFactory - load model from remote defoult", (done) => {
+  EfficientNetCheckPointFactory.create(EfficientNetCheckPoint.B0, {
+    localModelRootDirectory: rootDir,
+  })
+    .then(async (model) => {
+      expect(model).toBeDefined();
+      const image = "samples/car.jpg";
+      model.inference(image).then((predictions) => {
+        expect(predictions.result[0].label).toEqual("sports car, sport car");
+        done();
+      });
+    })
+    .catch((error) => {
       done(error);
     });
 });
