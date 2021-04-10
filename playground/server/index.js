f7979e3c9295e425b719098256fead723c6a4b8a
Naor Tedgi <naor.tedgi@gmail.com>
2021-04-10 10:54:10 +0300

    adding missing port

--- playground/server/index.js
+++ playground/server/index.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-undef
-
 import { createServer } from "./server.js";
+const port = 3000;
 createServer()
-    .then((app) => {
-        app.listen(port, () => {
-            console.log("Server is running on PORT", port);
-        });
-    }).catch((err) => {
-        console.log(err)
+  .then((app) => {
+    app.listen(port, () => {
+      console.log("Server is running on PORT", port);
     });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
