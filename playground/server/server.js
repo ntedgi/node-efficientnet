f0f821ff60b535d438385add6508a98f882c1a23
TomShlaim <tom.shlaim@ironsrc.com>
2022-09-27 17:59:17 +0300

    Handled the FED side Changes the BED api/upload logic

--- playground/server/server.js
+++ playground/server/server.js
@@ -1,77 +1,82 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable no-undef */
-import express, { Router } from "express";
-import bodyParser from "body-parser";
-import formidable from "express-formidable";
-import bodyParserErrorHandler from "express-body-parser-error-handler";
+import express, { Router } from 'express'
+import bodyParser from 'body-parser'
+import formidable from 'express-formidable'
+import bodyParserErrorHandler from 'express-body-parser-error-handler'
 
-
-const { urlencoded, json } = bodyParser;
+const { urlencoded, json } = bodyParser
 
 import {
   EfficientNetCheckPointFactory,
   EfficientNetCheckPoint,
   EfficientNetLanguageProvider,
-  EfficientNetLableLanguage
-} from "node-efficientnet";
+  EfficientNetLableLanguage,
+} from 'node-efficientnet'
 
 const initServer = (model) => {
 
-  const app = express();
-  const router = Router();
-  const serverName = "back-end";
+  const app = express()
+  const router = Router()
+  const serverName = 'back-end'
   app.use((req, res, next) => {
-    console.info(`${serverName} |  ${req.url}  ${req.method} -- ${new Date()}`);
-    next();
-  });
+    console.info(`${serverName} |  ${req.url}  ${req.method} -- ${new Date()}`)
+    next()
+  })
 
-  router.post("/api/upload", async (req, res) => {
+  router.post('/api/upload/:language', async (req, res) => {
     try {
-      const language = typeof req.fields.language;
-      const labelLanguage =  EfficientNetLableLanguage[EfficientNetLableLanguage[language]];
-      const languageProvider = new EfficientNetLanguageProvider(labelLanguage);
+      const language = !!req.params.language ? req.params.language : "english";
+      const formattedLanguage = language.toUpperCase();
+      const labelLanguage = EfficientNetLableLanguage[EfficientNetLableLanguage[formattedLanguage]]
+      const languageProvider = new EfficientNetLanguageProvider(labelLanguage)
 
-      model.setLanguageProvider(languageProvider);
+      model.setLanguageProvider(languageProvider)
 
-      const result = await model.inference(req.files.file.path);
-      res.send(result);
+      const result = await model.inference(req.files.file.path)
+      res.send(result)
     }
     catch (err) {
-      console.error(err);
-      res.status(500).send("Something went wrong");
-    }
-  });
-
-  router.get("/api/languages", async(req,res)=>{
-    try{
-      const languagesEnumKeys = Object.keys(EfficientNetLableLanguage);
-      const languagesAmount = languagesEnumKeys.length/2;
-      const languagesArr = languagesEnumKeys.slice(languagesAmount);
-      res.send(languagesArr);
-    }
-    catch(err){
-      console.error(err);
-      res.status(500).send("Something went wrong");
+      console.error(err)
+      res.status(500).send('Something went wrong')
     }
   })
 
-  router.get("/api/version", async (req, res) => {
-    res.send({ version: "1.0" });
-  });
+  router.get('/api/languages', async (req, res) => {
+    try {
+      const languagesEnumKeys = Object.keys(EfficientNetLableLanguage)
+      const languagesAmount = languagesEnumKeys.length / 2
+      const languagesArr = languagesEnumKeys.slice(languagesAmount)
 
-  app.use(urlencoded({ extended: true }));
-  app.use(json());
-  app.use(formidable());
-  app.use(bodyParserErrorHandler());
-  app.use(router);
-  return app;
-};
+      const formattedLanguagesArr = languagesArr.map(
+        language => language.toLowerCase()).
+        map(item => item.charAt(0).toUpperCase() + item.slice(1));
+
+      res.send(formattedLanguagesArr)
+    }
+    catch (err) {
+      console.error(err)
+      res.status(500).send('Something went wrong')
+    }
+  })
+
+  router.get('/api/version', async (req, res) => {
+    res.send({ version: '1.0' })
+  })
+
+  app.use(urlencoded({ extended: true }))
+  app.use(json())
+  app.use(formidable())
+  app.use(bodyParserErrorHandler())
+  app.use(router)
+  return app
+}
 
 const createServer = async () => {
   const model = await EfficientNetCheckPointFactory.create(
-    EfficientNetCheckPoint.B7
-  );
-  return await initServer(model);
-};
+    EfficientNetCheckPoint.B7,
+  )
+  return await initServer(model)
+}
 
-export { createServer };
+export { createServer }
--- playground/ui/src/app/imageuploader/imageuploader.component.ts
+++ playground/ui/src/app/imageuploader/imageuploader.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { MessageService } from 'primeng/api';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {MessageService, SelectItem} from 'primeng/api';
+import {HttpClient} from '@angular/common/http';
 import Prediction from '../interfces';
+import {LanguagesApiService} from '../languages-api.service'
 
 @Component({
   selector: 'app-imageuploader',
@@ -10,7 +11,12 @@ import Prediction from '../interfces';
   providers: [MessageService, HttpClient],
 })
 export class ImageuploaderComponent implements OnInit {
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService, private languagesApi: LanguagesApiService) {
+  }
+
+  languages: SelectItem[];
+  selectedLanguage: SelectItem;
+  selectedLanguageName: string;
 
   uploadedFiles: any[] = [];
 
@@ -18,7 +24,18 @@ export class ImageuploaderComponent implements OnInit {
   @Output() updateClassification = new EventEmitter<Prediction[]>();
   @Output() isLoading = new EventEmitter<boolean>();
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.languagesApi.getLanguages().subscribe((data) => {
+      const languagesArray = Object.values(data);
+      this.languages = languagesArray.map(language => ({label: language, value: null}));
+      this.selectedLanguage = this.languages[0];
+      this.selectedLanguageName = this.getSelectedLanguageName();
+    });
+  }
+
+  onChange(event) {
+    this.selectedLanguageName = this.getSelectedLanguageName();;
+  }
 
   onUpload(event) {
     const result = event.originalEvent.body.result as Prediction[];
@@ -36,5 +53,8 @@ export class ImageuploaderComponent implements OnInit {
     this.isLoading.emit(false);
     this.uploadedFiles = [];
     this.updateImage.emit(event.formData.get('file'));
+  }
+  getSelectedLanguageName(){
+    return this.selectedLanguage.label.toLowerCase();
   }
 }
