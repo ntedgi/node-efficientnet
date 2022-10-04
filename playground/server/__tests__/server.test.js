cb174701255089a3ecbf8287018fd78e3c75ba5b
Nancy <62155216+nancykwantify@users.noreply.github.com>
2022-10-04 09:28:08 -0400

    Update server.test.js

--- playground/server/__tests__/server.test.js
+++ playground/server/__tests__/server.test.js
@@ -10,7 +10,6 @@ beforeAll(async (done) => {
 }, 60000);
 
 describe("Post Endpoints", () => {
-
     it("/api/upload should throw exception when not passing a file", async (done) => {
         jest.setTimeout(60000);
         const res = await request(app)
@@ -29,37 +28,37 @@ describe("Post Endpoints", () => {
             });
     });
     it("should predict simple gold fish", async (done) => {
-        jest.setTimeout(60000);
-        const filePath = path.join(__dirname, "fish.jpg");
-        const res = await request(app)
-            .post("/api/upload")
-            .attach("file", filePath)
-            .expect(200)
-            .then((output, err) => {
-                const expectedLabel = "goldfish, Carassius auratus"
-                if (output) {
-                    const { result } = output.body;
-                    expect(result[0].label).toEqual(expectedLabel)
-                    done()
-                }
-                else {
-                    done(err)
-                }
-            })
-            .catch((err, res) => {
-                done(err);
-            });
-    });
-    it("sanity test server/version", async (done) => {
-        const res = await request(app)
-            .get("/api/version/")
-            .expect(200)
-            .then((response) => {
-                const version = response.body;
-                done();
-            })
-            .catch((err, res) => {
-                done(err);
-            });
-    });
+    jest.setTimeout(60000);
+    const filePath = join(__dirname, "fish.jpg");
+    await request(app)
+      .post("/api/upload")
+      .attach("file", filePath)
+      .expect(200)
+      .then((output, err) => {
+        const expectedLabel = "goldfish, Carassius auratus";
+        if (output) {
+          const { result } = output.body;
+          expect(result[0].label).toEqual(expectedLabel);
+          done();
+        } else {
+          done(err);
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+  it("sanity test server/version", async (done) => {
+    await request(app)
+      .get("/api/version/")
+      .expect(200)
+      .then((response) => {
+        const version = response.body;
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
 });
