94c5b2c3182df0d52dedd7c578d084881498ad76
TomShlaim <tom.shlaim@is.com>
2022-10-09 12:04:26 +0300

    Fixed problems due to the merge from master

--- playground/server/__tests__/server.test.js
+++ playground/server/__tests__/server.test.js
@@ -28,64 +28,9 @@ describe("Post Endpoints", () => {
             });
     });
     it("should predict simple gold fish", async (done) => {
-        jest.setTimeout(60000);
-        const filePath = path.join(__dirname, "fish.jpg");
-        const res = await request(app)
-            .post("/api/upload/english")
-            .attach("file", filePath)
-            .expect(200)
-            .then((output, err) => {
-                const expectedLabel = "goldfish, Carassius auratus"
-                if (output) {
-                    const { result } = output.body;
-                    expect(result[0].label).toEqual(expectedLabel)
-                    done()
-                }
-                else {
-                    done(err)
-                }
-            })
-            .catch((err, res) => {
-                done(err);
-            });
-    });
-  it("should predict simple gold fish in spanish", async (done) => {
-    jest.setTimeout(60000);
-    const filePath = path.join(__dirname, "fish.jpg");
-    const res = await request(app)
-    .post("/api/upload/spanish")
-    .attach("file", filePath)
-    .expect(200)
-    .then((output, err) => {
-      const expectedLabel = "pez dorado, Carassius auratus"
-      if (output) {
-        const { result } = output.body;
-        expect(result[0].label).toEqual(expectedLabel)
-        done()
-      }
-      else {
-        done(err)
-      }
-    })
-    .catch((err, res) => {
-      done(err);
-    });
-  });
-    it("sanity test server/version", async (done) => {
-        const res = await request(app)
-            .get("/api/version/")
-            .expect(200)
-            .then((response) => {
-                const version = response.body;
-                done();
-            })
-            .catch((err, res) => {
-                done(err);
-            });
-    });
-    jest.setTimeout(60000);
-    const filePath = join(__dirname, "fish.jpg");
-    await request(app)
+      jest.setTimeout(60000);
+      const filePath = join(__dirname, "fish.jpg");
+      await request(app)
       .post("/api/upload")
       .attach("file", filePath)
       .expect(200)
@@ -102,9 +47,30 @@ describe("Post Endpoints", () => {
       .catch((err) => {
         done(err);
       });
-  });
-  it("sanity test server/version", async (done) => {
-    await request(app)
+    });
+    it("should predict simple gold fish in spanish", async (done) => {
+      jest.setTimeout(60000);
+      const filePath = join(__dirname, "fish.jpg");
+      await request(app)
+      .post("/api/upload/spanish")
+      .attach("file", filePath)
+      .expect(200)
+      .then((output, err) => {
+        const expectedLabel = "pez dorado, Carassius auratus";
+        if (output) {
+          const { result } = output.body;
+          expect(result[0].label).toEqual(expectedLabel);
+          done();
+        } else {
+          done(err);
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+    });
+    it("sanity test server/version", async (done) => {
+      await request(app)
       .get("/api/version/")
       .expect(200)
       .then((response) => {
@@ -114,6 +80,6 @@ describe("Post Endpoints", () => {
       .catch((err) => {
         done(err);
       });
-  });
+    });
 
 });
