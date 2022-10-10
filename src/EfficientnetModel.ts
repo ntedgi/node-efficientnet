9f9c6da18926b627e06f7d7ba5b36050fa6e4784
TomShlaim <tom.shlaim@is.com>
2022-10-10 15:04:59 +0300

    Fixed merge issue

--- src/EfficientnetModel.ts
+++ src/EfficientnetModel.ts
@@ -118,7 +118,4 @@ export default class EfficientNetModel {
     const tensor = await this.createTensor(image);
     return this.predict(tensor, topK);
   }
-  setLanguageProvider(languageProvider: EfficientNetLanguageProvider){
-    this.languageProvider = languageProvider;
-  }
 }
